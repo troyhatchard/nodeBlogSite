808e301885f9baa7c02605691d9a95dbff32f0e2
troyhatch12 <troyhatchard@gmail.com>
2019-09-17 17:37:07 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//nodejs blog site
+
+const express = require('express');
+const app = express();
+const port = 3011;
+
+app.get('/', (req, res) => res.send('Welcome to Troy\'s Website!'));
+
+app.listen(port, () => console.log('Blog site is listening on port $[port]!'));
