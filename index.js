f125afdaf5f3d9c0718dfad713662d21e5b815a2
troyhatch12 <troyhatchard@gmail.com>
2019-09-17 18:04:35 -0600

    Changed to send html file

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const express = require('express');
 const app = express();
 const port = 3011;
 
-app.get('/', (req, res) => res.send('Welcome to Troy\'s Website!'));
+app.get('/', (req, res) => res.sendFile('index.html'));
 
 app.listen(port, () => console.log('Blog site is listening on port ${port}!'));
