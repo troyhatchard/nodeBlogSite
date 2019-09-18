cbaae013ac2d37f182ee08cb6a0465794220d0fb
troyhatch12 <troyhatchard@gmail.com>
2019-09-17 18:10:13 -0600

    Added require path

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app = express();
 const port = 3011;
+const path = require('path');
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 
