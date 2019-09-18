6c51d375914a96ce878e6c63f3074c7aee39b60b
troyhatch12 <troyhatchard@gmail.com>
2019-09-17 18:07:39 -0600

    path error

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const express = require('express');
 const app = express();
 const port = 3011;
 
-app.get('/', (req, res) => res.sendFile('index.html'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 
 app.listen(port, () => console.log('Blog site is listening on port ${port}!'));
