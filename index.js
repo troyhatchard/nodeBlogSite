3fe26e49fed0cb59804e038e9eec83e71fbe74c0
troyhatch12 <troyhatchard@gmail.com>
2019-09-20 16:44:06 -0600

    Added routes dir

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
 
 const express = require('express');
 const app = express();
-const port = 3011;
+const port = process.env.PORT || 3011;
 const path = require('path');
+
+//routes
+//const blog = './routes/blog';
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 
-app.listen(port, () => console.log('Blog site is listening on port ${port}!'));
+app.get('/blog', (req, res) => res.sendFile(path.join(__dirname + '/routes/blogpost.html')));
+
+app.listen(port, () => console.log(`Blog site is listening on port ${port}!`));
