98da6b0b2f342b00c92ecbf87837173adb3d154b
troyhatch12 <troyhatchard@gmail.com>
2019-09-28 13:33:30 -0600

    Installed and added Pug
    Added pug. Replaced html document with a .pug doc in the views folder.
    I'm pumped to add dynamic content with pug

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const path = require('path');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+app.set('view engine', 'pug'); //don't have to require this because it loads itself automatically
+
 console.log('Application Name: ' + config.get('name'));
 console.log('DB Password: ' + config.get('db.password'));
 nodeEnv = app.get('env');
@@ -23,7 +25,9 @@ if (nodeEnv == 'development'){
 //routes
 //const blog = './routes/blog';
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
+app.get('/', (req, res) => {
+    res.render('index', {title: 'Blog Site', message: 'Welcome to Troy\'s website with Pug!'})
+});
 
 app.get('/blog', (req, res) => res.sendFile(path.join(__dirname + '/routes/blogpost.html')));
 
