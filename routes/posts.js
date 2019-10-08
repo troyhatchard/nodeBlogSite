f8cc17bad3f08b6d8f1843008222466679cc2a6f
troyhatch12 <troyhatchard@gmail.com>
2019-10-08 00:06:18 -0600

    File display sort of working
    File display works but I can't dynamically assign a filename to it. So
    I'm unable to load different files based on the URL with the same pug
    page.

--- routes/posts.js
+++ routes/posts.js
@@ -8,7 +8,17 @@ router.get('/', (req, res) => {
 });
 
 router.get('/test', (req, res) => {
-    res.render('blogPostTest', {title: 'Blog Site - Test', postTitle: 'My Test Post', postContent: 'This is a sample blog post. \lThis is a test on a new line'})
+    res.render('blogPostTest', {title: 'Blog Site - Test', postTitle: 'My Test Post', postContent: 'This is a sample blog post. This is a test on a new line'})
+});
+
+router.get('/blogFile', (req, res) => {
+    res.render('blogPostFile', {title: 'Blog Site - ', postTitle: 'My Test Post', postContent: 'This is a sample blog post. This is a test on a new line'})
+});
+
+router.get('/:id', (req, res) => {
+    id = req.params.id;
+    title = `Blog Post ${id}`;
+    res.render('blogPostFile', {title: title, postTitle: title, postId:id})
 });
 
 module.exports = router;
